b3f9cbe7f5030cfb50f694ad79008692f3cefee3
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-07-27 18:04:37 -0400

    removed test files...

--- src/scripts/Test/test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Another = props => (
-    <React.Fragment>
-        <div id='100'>{props}</div>
-        <div id='100'>{props}</div>
-    </React.Fragment>
-)
--- src/scripts/test.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-const Test = props => {
-    return <div id='10'>Hello {props}</div>
-}
-export default Test
