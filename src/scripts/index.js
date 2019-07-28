910fd92c78632cd083002b306ef57e14256cfd97
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-07-28 18:43:55 -0400

    test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,1 @@
 console.log('Environment:', process.env.NODE_ENV)
-
-const x =
-    '1234567890' +
-    '1234567890' +
-    '1234567890' +
-    '1234567890' +
-    '1234567890' +
-    '1234567890' +
-    '1234567890'
-
-const y = props => props
