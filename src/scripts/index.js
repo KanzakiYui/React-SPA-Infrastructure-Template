938e24efdf47aa204b929e18af19b4651d4ec59c
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-07-28 18:17:18 -0400

    Test lint-staged

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
 console.log('Environment:', process.env.NODE_ENV)
+
+const X =
+    '1234567890' +
+    '1234567890' +
+    '1234567890' +
+    '1234567890' +
+    '1234567890' +
+    '1234567890'
+
+const AddMore = props => !props
