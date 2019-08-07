a5b5e4960ef886f4e9cf41bb37c29ee56be2e804
Yui <yaopeng.liu@mail.utoronto.ca>
2019-08-06 21:11:41 -0400

    fixed resolve bug in webpack

--- compile/resolve.js
+++ compile/resolve.js
@@ -1,9 +1,9 @@
 const path = require('path')
 module.exports = {
     modules: [
-        path.resolve(__dirname, '../scr/scripts'),
-        path.resolve(__dirname, '../scr/scss'),
-        path.resolve(__dirname, '../scr/media'),
+        path.resolve(__dirname, '../src/scripts'),
+        path.resolve(__dirname, '../src/scss'),
+        path.resolve(__dirname, '../src/media'),
         path.resolve(__dirname, '../node_modules'),
     ],
     extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx']
